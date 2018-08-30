a0291038f26a32a43e8f7d686b69dfc92e5797c8
Miki <varga.misha@gmail.com>
2018-08-30 13:18:51 +0300

    select right way to save ways

--- src/select_ways.c
+++ src/select_ways.c
@@ -1,16 +1,6 @@
 #include "../inc/lem_in.h"
 #include <stdio.h>
-
-static int *save_ways(t_map *pmap, int *old_arr, int data)
-{
-    int *arr_way;
-
-    //join_arr;
-
-    free(old_arr);
-    return arr_way;
-}
-
+/*
 static void save_ways(t_map *pmap, int *arr_way)//needed check if the romm is new
 {
     int **pp;
@@ -32,26 +22,26 @@ static void save_ways(t_map *pmap, int *arr_way)//needed check if the romm is ne
     if (pmap->number_of_ways)
         free(old_ways - (pmap->number_of_ways));
     pmap->ways = pp - (pmap->number_of_ways + 1);
-}
+}*/
 
-static char *create_veretx(t_map *pmap)
+static int *create_arr(t_map *pmap, int data)
 {
-    char *nodes;
+    int *nodes;
     int i;
 
     i = 0;
-    nodes = (char *)malloc(sizeof(char) * pmap->number_of_rooms);
+    nodes = (int *)malloc(sizeof(int) * pmap->number_of_rooms);
     if (!nodes)
     {
         ft_putendl("Unable to allocate memory!");
         exit(EXIT_FAILURE);
     }
     while (i < pmap->number_of_rooms)
-        nodes[i++] = ORANGE;
+        nodes[i++] = data;
     return nodes;
 }
 
-static void save_way(t_map *pmap, struct s_stack **root, char *nodes, int i)
+static void save_way(t_map *pmap, struct s_stack **root, int *nodes, int i)
 {
     t_queue queue;
     t_edge edge;
@@ -84,14 +74,22 @@ static void save_way(t_map *pmap, struct s_stack **root, char *nodes, int i)
 t_boolean check_ways(t_map *pmap)
 {
     struct s_stack *stack = NULL;
-    char *nodes;
+    int *nodes;
+    //int *ways;
     t_edge edge;
     int i;
 
+    struct s_stack *st = NULL;
+    //int j;
     i = pmap->index_end;
-    nodes = create_veretx(pmap);
+    nodes = create_arr(pmap, ORANGE);
+//ways = create_arr(pmap, (i = pmap->index_end));
     save_way(pmap, &stack, nodes, i);
     free(nodes);
+    //j = pmap->number_of_rooms;
+    //ways[0] = i;
+    edge.start = i;
+    s_push(&st, edge);
     printf("!%s ", pmap->the_rooms[i]);//
     while (!s_is_empty(stack))
     {
@@ -100,11 +98,27 @@ t_boolean check_ways(t_map *pmap)
         if (edge.end == i)
         {
             i = edge.start;
+            s_push(&st, edge);
+            //ways[--j] = i;
             printf(" <- %s", pmap->the_rooms[i]); //
             //save_arr_vays;
             clear_room_in_matrix(pmap, i);
         }
     }
+    printf("\n");
+
+    t_edge e;
+    while(!s_is_empty(st))
+    {
+        e = s_peek(st);
+        s_pop(&st);
+        printf("%s > ", pmap->the_rooms[e.start]);
+    }
+    /*int z = 0;
+    while (z < pmap->number_of_rooms)
+    {
+        printf("%s > ", pmap->the_rooms[ways[z++]]);
+     }*/
     printf("\n"); //
     //save_ways
     return (i == pmap->index_end ? FALSE : TRUE);
